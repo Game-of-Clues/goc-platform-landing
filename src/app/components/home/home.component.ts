cbf1bd786f3c77bc7e39f8a8f8d1954ad75beb31
ViktorVukev <vikivukev@gmail.com>
2024-09-07 00:04:14 +0300

    Adding FAQ and Team components, fixing minor homepage bugs on mobile version

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import { HeaderComponent } from '../../shared/header/header.component';
+import { ContactComponent } from "../contact/contact.component";
+import { LastComponent } from '../last/last.component';
+import { TeamComponent } from '../team/team.component';
+import { FaqComponent } from '../faq/faq.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink, HeaderComponent, ContactComponent, LastComponent, TeamComponent, FaqComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
